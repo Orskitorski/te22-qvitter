9dd5fd4787bddaab10feba2b4a8ae3fa70133258
OrskiTorski <oscar.tafvelin@elev.ga.ntig.se>
2025-03-09 14:06:34 +0100

    allting klart

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,11 +1,15 @@
 import express from "express"
 import pool from "../db.js"
+import { ExpressValidator } from "express-validator"
 
 const router = express.Router()
 
 router.get("/", async (req, res)=> {
 
-    const [tweets] = await pool.promise().query(`SELECT tweet.*, user.name FROM tweet JOIN user ON tweet.author_id = user.id ORDER BY created_at DESC;`)
+    const [tweets] = await pool.promise().query(
+        `SELECT tweet.*, user.name FROM tweet 
+        JOIN user ON tweet.author_id = user.id 
+        ORDER BY created_at DESC;`)
    
     res.render("tweets.njk", {
         message: "Welcome to Qvitter!",
@@ -13,16 +17,43 @@ router.get("/", async (req, res)=> {
     })
 })
 
+router.get("/:id/favourites", async (req, res) => {
+    const id = req.params.id
+
+    if (!Number.isInteger(Number(id))) {
+        return res.status(400).send("Invalid ID")
+    }
+
+    await pool.promise().query('INSERT INTO favourites (tweet_id, user_id) VALUES (?, ?)', [id, 1])
+
+    res.redirect("http://localhost:3000/tweets/favourites")
+})
+
 router.get("/favourites", async (req, res)=> {
 
-    const [favourites] = await pool.promise().query(`SELECT favourites.*, tweet.message FROM favourites JOIN tweet ON favourites.id = tweet.id ORDER BY created_at DESC;`)
-    const [fav_user] = await pool.promise().query(`SELECT favourites.*, user.name FROM favourites JOIN user ON favourites.user_id = user.id;`)
+    const [favourites] = await pool.promise().query(
+        `SELECT * FROM favourites 
+        JOIN tweet ON favourites.tweet_id = tweet.id 
+        JOIN user ON tweet.author_id = user.id 
+        ORDER BY created_at DESC;`)
 
     res.render("favourites.njk", {
         message: "Favourite Qveets",
         favourites: favourites,
-        fav_user: fav_user
     })
+})
+
+router.get("/:id/unfavourite", async (req, res) => {
+    const id = req.params.id
+    console.log(id)
+
+    if (!Number.isInteger(Number(id))) {
+        return res.status(400).send("Invalid ID")
+    }
+
+    await pool.promise().query(`DELETE FROM favourites WHERE tweet_id = ?`, [id])
+
+    res.redirect("http://localhost:3000/tweets/favourites")
 })
 
 router.get("/:id/delete", async (req, res) => {
@@ -40,6 +71,10 @@ router.get("/:id/delete", async (req, res) => {
 
 router.get("/:id/edit", async (req, res) => {
     const id = req.params.id
+
+    if (!Number.isInteger(Number(id))) {
+        return res.status(400).send("Invalid ID")
+    }
     
     const [rows] = await pool.promise().query("SELECT * FROM tweet WHERE id = ?", [id])
     if (rows.length === 0) {
@@ -58,7 +93,7 @@ router.post('/edit', async (req, res) => {
     
     const { message, id } = req.body
 
-    await pool.promise().query('UPDATE tweet SET message=? where id=?', [message, id])
+    await pool.promise().query(`UPDATE tweet SET message=? where id=?`, [message, id])
 
     res.redirect("http://localhost:3000/tweets")
 })
@@ -75,7 +110,7 @@ router.get("/post", async (req, res) => {
 router.post('/', async (req, res) => {  
     const { author_id, message } = req.body
 
-    const [tweet] = await pool.promise().query('insert into tweet (author_id, message) values (?, ?)', [author_id, message])
+    await pool.promise().query(`INSERT INTO tweet (author_id, message) values (?, ?)`, [author_id, message])
 
     res.redirect("http://localhost:3000/tweets")
 })
